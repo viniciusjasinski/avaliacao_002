bb12ea4a9b332ea1ee41b0841700cd84d12047fc
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-08-27 17:09:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/avaliacao2githubapi/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.avaliacao2githubapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.avaliacao2githubapi.view.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/avaliacao2githubapi/view_model/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.avaliacao2githubapi.view_model
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
