7ff0300995706b9c3f4b5c472cdacfd0be414370
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-08-28 22:59:03 -0300

    Creating the details adapter

---
+++ app/src/main/java/com/example/avaliacao2githubapi/adapter/PullRequestsDetailsAdapter.kt
@@ -0,0 +1,55 @@
+package com.example.avaliacao2githubapi.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.avaliacao2githubapi.R
+import com.example.avaliacao2githubapi.databinding.ItemRepositoryBinding
+import com.example.avaliacao2githubapi.model.DescriptionRepository
+
+class PullRequestsDetailsAdapter(val onClickPullRequest: (DescriptionRepository) -> Unit) :
+    RecyclerView.Adapter<ItemPullRequestsDetailsViewHolder>() {
+
+    private val listOfPullRequests: MutableList<DescriptionRepository> = mutableListOf()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ItemPullRequestsDetailsViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_pull_details, parent, false)
+        return ItemPullRequestsDetailsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemPullRequestsDetailsViewHolder, position: Int) {
+        listOfPullRequests[position].apply {
+            holder.bind(this)
+            holder.itemView.setOnClickListener {
+                onClickPullRequest(this)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listOfPullRequests.size
+    }
+
+    fun refresh(newList: List<DescriptionRepository>) {
+        listOfPullRequests.clear()
+        listOfPullRequests.addAll(newList)
+        notifyDataSetChanged()
+    }
+
+}
+
+
+class ItemPullRequestsDetailsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val binding: ItemRepositoryBinding = ItemRepositoryBinding.bind(itemView)
+
+    fun bind(descriptionRepository: DescriptionRepository) {
+
+    }
+
+}
