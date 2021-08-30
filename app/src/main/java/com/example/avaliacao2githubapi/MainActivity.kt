89c491d0046fe4685f0b016f09377bad9cb35ab1
viniciusjasinski <vinicius.jasinski@gmail.com>
2021-08-29 23:13:35 -0300

    Creating a splash activity and setting to start the application on that

--- app/src/main/java/com/example/avaliacao2githubapi/MainActivity.kt
+++ app/src/main/java/com/example/avaliacao2githubapi/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         changeFragments(AllRepositoriesFragment.newInstance())
     }
 
---
+++ app/src/main/java/com/example/avaliacao2githubapi/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.avaliacao2githubapi
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+}
